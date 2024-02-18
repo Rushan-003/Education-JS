78a5db9e02e84b6451e7405a547893886b803fab
Rushan_Hamidullin <xamidullin.rushan@gmail.com>
2024-02-18 21:04:46 +0300

    0.1

---
+++ app/app.js
@@ -0,0 +1,6 @@
+let plohieSlova = ["дурачёк", "дебил", "тупой"]
+let diestvie = ["ударить об стенку", "выбросить из окна"]
+
+let draznilka = ["Ты такой", plohieSlova[Math.floor(Math.random() * plohieSlova.length)], ", что тебя хочется", diestvie[Math.floor(Math.random() * diestvie.length)]]
+
+console.log(draznilka);
